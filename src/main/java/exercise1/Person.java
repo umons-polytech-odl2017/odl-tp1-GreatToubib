e5ef76b1d34e26508299376c18ffe2d149ebce01
GreatToubib <150543@umons.ac.be>
2017-10-13 15:39:31 +0200

    Work in progress

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,44 @@
 package exercise1;
 
-class Person {
+public class Person {
+	private String name;
+	private int age;
+
+	// constructeur
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	//getters
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
 }
+/*
+	// constructeur
+	public Person (){
+		age=15;
+		String name = "juda";
+	}
+
+	protected Person(String name, int age){
+		this.age = age;
+		this.name = name;
+	}
+
+	// accesseur
+
+	public int getAge() {return age;}
+
+	public String getName() {return name;}
+
+}
+
+// alt + insert
+
